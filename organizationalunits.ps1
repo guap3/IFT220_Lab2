906cb9073a9909dcfe3c1011a9860baba3b5cb7f
guap3 <48692135+guap3@users.noreply.github.com>
2019-04-27 17:28:09 -0500

    Renamed OU.ps1 to organizational units.ps1

--- organizationalunits.ps1
+++ organizationalunits.ps1
@@ -4,7 +4,7 @@
 
 Write-Host -ForegroundColor yellow "Creating OUs"
 New-ADOrganizationalUnit -Name ST_Computers -Path "DC=AD,DC=sparky-tech,DC=com" -Description "ST Computers"
- New-ADOrganizationalUnit -Name Workstations -Path "OU=ST_Computers,DC=AD,DC=sparky-tech,DC=com"
+New-ADOrganizationalUnit -Name Workstations -Path "OU=ST_Computers,DC=AD,DC=sparky-tech,DC=com"
   New-ADOrganizationalUnit -Name President -Path "OU=Workstations,OU=ST_Computers,DC=AD,DC=sparky-tech,DC=com"
   New-ADOrganizationalUnit -Name Finance -Path "OU=Workstations,OU=ST_Computers,DC=AD,DC=sparky-tech,DC=com"
   New-ADOrganizationalUnit -Name HR -Path "OU=Workstations,OU=ST_Computers,DC=AD,DC=sparky-tech,DC=com"
